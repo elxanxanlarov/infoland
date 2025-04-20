3b57db368dceeb4dfb1bde52da95a24cb763b099
Elxan <elxanxanlarov@gmail.com>
2025-04-20 22:33:59 +0400

    refactor(layout): update page, footer, and navbar components for consistency
    This commit includes changes to the page, footer, and navbar components to ensure consistency across the application. The content of these components has been simplified, and the navbar has been enhanced with a responsive design using a sheet component for mobile view. Additionally, the layout now uses the Inter font instead of Geist for better readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased light`}
+        className={`${interSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextIntlClientProvider>
           <Navbar/>
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-[var(--white)]">
-      Navbar
+    <nav className="bg-[var(--white)] w-screen">
+      <div className="mx-auto w-4/5 py-4 flex justify-between items-center">
+        <Link href="/" className="text-[var(--black)] text-2xl md:text-3xl ">
+          InfoLand
+        </Link>
+        <ul className=" hidden md:flex items-center gap-4">
+          <li><Link href={"/"}>Home</Link></li>
+          <li><Link href={"/"}>Blogs</Link></li>
+          <li><Link href={"/"}>Questions</Link></li>
+        </ul>
+        <div className="hidden md:flex items-center gap-4">
+          <Button type="button" className="cursor-pointer hover:bg-black hover:text-white" variant="outline">Login</Button>
+          <Button type="button" className="cursor-pointer bg-black text-white hover:bg-white hover:text-black" variant="outline">Register</Button>
+        </div>
+        <Sheet>
+          <SheetTrigger className="md:hidden cursor-pointer">
+            <Menu />
+          </SheetTrigger>
+          <SheetContent className="bg-white">
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+              <SheetDescription>
+                <ul className="flex flex-col gap-4">
+                  <li><Link href={"/"}>Home</Link></li>
+                  <li><Link href={"/"}>Blogs</Link></li>
+                  <li><Link href={"/"}>Questions</Link></li>
+                </ul>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
+
+      </div>
     </nav>
   );
 };
